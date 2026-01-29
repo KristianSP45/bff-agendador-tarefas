a00ee44c20e5ddd3abf705c4c856b0e754f67ccb
Kristian Pessoa <sousakristian45@gmail.com>
2026-01-29 17:03:29 -0300

    Adicionados comentarios pelo projeto

--- src/main/java/com/kristian/bffagendadortarefas/BffAgendadorTarefasApplication.java
+++ src/main/java/com/kristian/bffagendadortarefas/BffAgendadorTarefasApplication.java
@@ -3,11 +3,15 @@ package com.kristian.bffagendadortarefas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.swing.*;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableScheduling
+@EnableFeignClients//Ela diz: “Spring, procure interfaces anotadas com @FeignClient e transforme elas em beans.”
+@EnableScheduling//Ela diz: “Spring, procure métodos anotados com @Scheduled e execute eles automaticamente.”
 public class BffAgendadorTarefasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kristian/bffagendadortarefas/infractructure/client/config/FeignConfig.java
+++ src/main/java/com/kristian/bffagendadortarefas/infractructure/client/config/FeignConfig.java
@@ -3,11 +3,15 @@ package com.kristian.bffagendadortarefas.infractructure.client.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import javax.swing.*;
+
+@Configuration//“Essa classe define beans de configuração da aplicação”
 public class FeignConfig {
 
     @Bean
     public FeignError feignError(){
     return new FeignError();
     }
+    // FeignError traduz erros HTTP das APIs externas(client) em exceções do BFF
+    // FeignConfig registra esse tradutor para que o Feign o utilize automaticamente
 }
